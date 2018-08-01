3275085108420f7cea79844520cbd9c8f6edf05b
vhuhwavhohau <mavhandu3@ymail.com>
2018-08-01 14:32:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    MediaCapture,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureAudioOptions, CaptureVideoOptions } from '@ionic-native/media-capture';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private mediaCapture: MediaCapture) {
 
   }
+  Photo(){
+    let options: CaptureImageOptions = {limit:1};
+    this.mediaCapture.captureImage(options)
+    .then(
+      (data : MediaFile[]) => console.log(data),
+      (err: CaptureError) => console.error(err)
+    );
+  }
+  Audio(){
+    let options: CaptureAudioOptions = {limit:1};
+    this.mediaCapture.captureAudio(options)
+    .then(
+      (data : MediaFile[]) => console.log(data),
+      (err: CaptureError) => console.error(err)
+    );
+  }
+  Vedio(){
+    let options: CaptureVideoOptions = {limit:1};
+    this.mediaCapture.captureVideo(options)
+    .then(
+      (data : MediaFile[]) => console.log(data),
+      (err: CaptureError) => console.error(err)
+    );
+   }
 
 }
